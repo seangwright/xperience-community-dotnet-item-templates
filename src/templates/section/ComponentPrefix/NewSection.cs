56dba736d730149aed1a1bcf8ea3f265d55e8c4c
Sean G. Wright <seanw@kentico.com>
2023-06-24 12:09:01 -0400

    feat(section): add new template

---
+++ src/templates/section/ComponentPrefix/NewSection.cs
@@ -0,0 +1,52 @@
+using System.Threading.Tasks;
+using Kentico.Xperience.Admin.Base;
+using Kentico.Xperience.Admin.Base.FormAnnotations;
+using Kentico.PageBuilder.Web.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using MyApp.Web.Components.Sections.ComponentPrefix;
+
+[assembly: RegisterSection(
+    identifier: NewSection.IDENTIFIER,
+    viewComponentType: typeof(NewSection),
+    name: "displayName Section",
+    propertiesType: typeof(NewSectionProperties),
+    Description = "A displayName Section.",
+    IconClass = Icons.CustomElement)]
+
+namespace MyApp.Web.Components.Sections.ComponentPrefix;
+
+public class NewSection : ViewComponent
+{
+    public const string IDENTIFIER = "MyApp.Web.Components.Sections.NewSection";
+
+    public NewSection() { }
+
+    public async Task<IViewComponentResult> InvokeAsync(ComponentViewModel<NewSectionProperties> cvm)
+    {
+        var vm = new NewSectionViewModel(cvm.Properties);
+
+        await Task.CompletedTask;
+
+        return View(vm);
+    }
+}
+
+public class NewSectionProperties : ISectionProperties
+{
+    [TextInputComponent(
+        Label = "Label",
+        ExplanationText = "A custom label",
+        Tooltip = "Enter a label",
+        Order = 1)]
+    public string Label { get; set; } = "";
+}
+
+public class NewSectionViewModel
+{
+    public NewSectionViewModel(NewSectionProperties props)
+    {
+        Label = props.Label;
+    }
+
+    public string Label { get; } = "";
+}
