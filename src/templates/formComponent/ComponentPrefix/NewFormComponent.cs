52406c66b64d473a4d704bd4d774435cde8f24ee
Sean G. Wright <seanw@kentico.com>
2023-07-01 11:27:51 -0400

    feat(templates): FormComponent

---
+++ src/templates/formComponent/ComponentPrefix/NewFormComponent.cs
@@ -0,0 +1,44 @@
+using MyApp.Web.Components.Widgets.ComponentPrefix;
+using Kentico.Forms.Web.Mvc;
+using CMS.DataEngine;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+[assembly: RegisterFormComponent(
+    identifier: NewFormComponent.IDENTIFIER,
+    formComponentType: typeof(NewFormComponent),
+    name: "displayName Form Component",
+    Description = "A displayName Form Component.",
+    // https://devnet.kentico.com/docs/icon-list/index.html
+    IconClass = "icon-dialog-window-cogwheel",
+    IsAvailableInFormBuilderEditor = false,
+    IsMappableToContactFields = false,
+    ViewName = "~/Components/FormComponents/ComponentPrefix/NewFormComponent.cshtml")]
+
+namespace MyApp.Web.Components.Widgets.ComponentPrefix;
+
+public class NewFormComponent : FormComponent<NewFormComponentProperties, string>
+{
+    public const string IDENTIFIER = "MyApp.Web.Components.FormComponents.NewFormComponent";
+
+    [BindableProperty]
+    public string Value { get; set; }
+
+    public override string GetValue()
+    {
+        return Value;
+    }
+
+    public override void SetValue(string value)
+    {
+        Value = value;
+    }
+}
+
+public class NewFormComponentProperties : FormComponentProperties<string>
+{
+    public NewFormComponentProperties() : base(FieldDataType.Text, size: 200) { }
+
+    [DefaultValueEditingComponent(TextInputComponent.IDENTIFIER)]
+    public override string DefaultValue { get; set; } = "";
+}
+
